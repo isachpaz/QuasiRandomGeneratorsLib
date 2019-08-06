95009102d1dbb8a947ee229e57641eb3cfa701e7
Dr. Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2019-08-07 01:15:36 +0200

    RandomSequence class added

---
+++ QuasiRandomGeneratorsLib/QuasiRandomGeneratorsLib.Tests/RandomSequenceTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace QuasiRandomGeneratorsLib.Tests
+{
+    [TestFixture]
+    public class RandomSequenceTests
+    {
+        [Test]
+        public void ctor_Test()
+        {
+            RandomSequence rSeq = new RandomSequence(1000);
+            var values = rSeq.GetDoubles().Take(100);
+
+            Assert.AreEqual(values.Count(), 100);
+        }
+    }
+}
---
+++ QuasiRandomGeneratorsLib/QuasiRandomGeneratorsLib/RandomSequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuasiRandomGeneratorsLib
+{
+    public class RandomSequence
+    {
+        public int Seed { get; }
+        private Lazy<Random> _rnd;
+        public RandomSequence(int seed)
+        {
+            Seed = seed;
+            _rnd = new Lazy<Random>(() => new Random(seed));
+        }
+
+        public IEnumerable<double> GetDoubles()
+        {
+            while (true)
+            {
+                yield return _rnd.Value.NextDouble();
+            }
+        }
+    }
+}
