35c958fdb1590a2c51991a64caea48ad9f51574d
Dr. Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2019-08-06 22:42:32 +0200

    HaltinSequence3D added

---
+++ QuasiRandomGeneratorsLib/QuasiRandomGeneratorsLib/HaltonSequence3D.cs
@@ -0,0 +1,9 @@
+﻿namespace QuasiRandomGeneratorsLib
+{
+    public class HaltonSequence3D : HaltonSequence
+    {
+        public HaltonSequence3D() : base(dimension: 3)
+        {
+        }
+    }
+}
