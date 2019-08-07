6ebf9b201de1421a4d8c52af227f42a78289d155
Dr. Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2019-08-07 09:10:27 +0200

    MyUtilsLib project added

--- QuasiRandomGeneratorsLib/MyUtilsLib/MyUtils.cs
+++ QuasiRandomGeneratorsLib/MyUtilsLib/MyUtils.cs
@@ -15,12 +15,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #endregion
+
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace QuasiRandomGeneratorsLib
+namespace MyUtilsLib
 {
-    internal static class MyUtils
+    public static class MyUtils
     {
         public static IEnumerable<double> ToDouble(this IEnumerable<Tuple<long, long>> input)
         {
@@ -34,6 +36,14 @@ namespace QuasiRandomGeneratorsLib
         public static double ToDouble(this Tuple<long, long> input)
         {
             return (double) input.Item1 / (double) input.Item2;
+        }
+
+        public static IEnumerable<Tuple<T1, T2>> ZipTwoEnumerable<T1, T2>(IEnumerable<T1> list1, IEnumerable<T2> list2)
+        {
+            foreach (var tuple in list1.Zip(list2, Tuple.Create))
+            {
+                yield return tuple;
+            }
         }
     }
 }
