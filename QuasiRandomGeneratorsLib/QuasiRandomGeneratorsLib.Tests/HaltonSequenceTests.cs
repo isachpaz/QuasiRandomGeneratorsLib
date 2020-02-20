e80557f9edd7ba1f04e29f9aebd0972651457182
Dr. Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2020-02-21 00:08:14 +0100

    Code clean up

--- QuasiRandomGeneratorsLib/QuasiRandomGeneratorsLib.Tests/HaltonSequenceTests.cs
+++ QuasiRandomGeneratorsLib/QuasiRandomGeneratorsLib.Tests/HaltonSequenceTests.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MoreLinq.Extensions;
 using MyUtilsLib;
 using NUnit.Framework;
-
 
 namespace QuasiRandomGeneratorsLib.Tests
 {
