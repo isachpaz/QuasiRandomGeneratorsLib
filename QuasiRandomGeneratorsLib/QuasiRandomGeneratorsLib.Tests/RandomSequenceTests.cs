c6601e9b5f350c4c8646ae0ff79367a15f27cb06
Dr. Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2020-02-21 00:09:54 +0100

    Random_with_Seed_Test(...) and  Random_with_Seed_Exception_Test(...) added

--- QuasiRandomGeneratorsLib/QuasiRandomGeneratorsLib.Tests/RandomSequenceTests.cs
+++ QuasiRandomGeneratorsLib/QuasiRandomGeneratorsLib.Tests/RandomSequenceTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
 using NUnit.Framework;
 
 namespace QuasiRandomGeneratorsLib.Tests
@@ -10,9 +12,41 @@ namespace QuasiRandomGeneratorsLib.Tests
         public void ctor_Test()
         {
             RandomSequence rSeq = new RandomSequence(1000);
-            var values = rSeq.GetDoubles().Take(100);
+            var values = rSeq.GetDoubles().Take(100).ToList();
 
             Assert.AreEqual(values.Count(), 100);
+        }
+
+        [TestCase(10)]
+        [TestCase(100000)]
+        [TestCase(12342345)]
+        [TestCase(Int16.MaxValue)]
+        public void Random_with_Seed_Test(int seedMax)
+        {
+            Func<int> randomSeed = 
+                () => Helpers.SeedHelper.GetRandomSeed(
+                    new RNGCryptoServiceProvider(), seedMax);
+
+            RandomSequence  seq = new RandomSequence(seed: randomSeed);
+            var values = seq.GetDoubles().Take(100).ToList();
+            Assert.AreEqual(values.Count(), 100);
+            Assert.LessOrEqual(seq.Seed, seedMax);
+        }
+
+        [TestCase(-100)]
+        [TestCase(0)]
+        public void Random_with_Seed_Exception_Test(int seedMax)
+        {
+            Assert.Throws(typeof(ArgumentException), () =>
+            {
+                Func<int> randomSeed =
+                    () => Helpers.SeedHelper.GetRandomSeed(
+                        new RNGCryptoServiceProvider(),
+                        seedMax);
+
+                RandomSequence seq = new RandomSequence(seed: randomSeed);
+                //var values = seq.GetDoubles().Take(100).ToList();
+            });
         }
     }
 }
